1bd9535dd3aa5039757ddd0d19c9917c6944ac15
Ксюша <124886145+KsseniyaTerehova@users.noreply.github.com>
2023-05-01 21:47:14 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,67 +5,67 @@
 
 template<typename T>
 class TPQueue {
-  private:
-    struct Node {
-        T v;
-        Node* n;
-    };
-    Node* header;
-  public:
-    TPQueue() : header(nullptr) {}
-    T pop();
-    void push(const T& r);
- };
- template<typename T>
- void TPQueue<T>::push(const T& r) {
-     if (header == NULL) {
-         Node* str = new Node;
-         str->v = r;
-         str->n = NULL;
-         header = str;
-     } else {
-         Node* c1 = header;
-         Node* c2 = new Node;
-         int q = 0;
-         while (c1) {
-             if (c1->v.prior < r.prior) {
-                 break;
-             } else {
-                 c1 = c1->n;
-                 q++;
-             }
-         }
-         if (q == 0) {
-             c2->n = header;
-             c2->v = r;
-             header = c2;
-         } else {
-             c1 = header;
-             for (int i = 1; i < q; i++) {
-                 c1 = c1->n;
-             }
-             c2->n = c1->n;
-             c2->v = r;
-             c1->n = c2;
-         }
-     }
- }
- template<typename T>
- T TPQueue<T>::pop() {
-     if (header == NULL) {
-         throw std::string("!!!");
-     } else {
-         T r = header->v;
-         Node* c = header->n;
-         delete header;
-         header = c;
-         return r;
-     }
- }
+ private:
+   struct Node {
+       T v;
+       Node* n;
+   };
+   Node* header;
+ public:
+   TPQueue() : header(nullptr) {}
+   T pop();
+   void push(const T& r);
+};
+template<typename T>
+void TPQueue<T>::push(const T& r) {
+    if (header == NULL) {
+        Node* str = new Node;
+        str->v = r;
+        str->n = NULL;
+        header = str;
+    } else {
+        Node* c1 = header;
+        Node* c2 = new Node;
+        int q = 0;
+        while (c1) {
+            if (c1->v.prior < r.prior) {
+                break;
+            } else {
+                c1 = c1->n;
+                q++;
+            }
+        }
+        if (q == 0) {
+            c2->n = header;
+            c2->v = r;
+            header = c2;
+        } else {
+            c1 = header;
+            for (int i = 1; i < q; i++) {
+                c1 = c1->n;
+            }
+            c2->n = c1->n;
+            c2->v = r;
+            c1->n = c2;
+        }
+    }
+}
+template<typename T>
+T TPQueue<T>::pop() {
+    if (header == NULL) {
+        throw std::string("!!!");
+    } else {
+        T r = header->v;
+        Node* c = header->n;
+        delete header;
+        header = c;
+        return r;
+    }
+}
 
- struct SYM {
-   char ch;
-   int prior;
- };
+struct SYM {
+  char ch;
+  int prior;
+};
 
 #endif  // INCLUDE_TPQUEUE_H_
